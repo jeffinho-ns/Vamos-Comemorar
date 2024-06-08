8730b2c735d8c54ffd30ae3037bf813e5d39a19b
Jefferson Lima <82480661+jeffinho-ns@users.noreply.github.com>
2024-06-08 15:55:52 -0300

    Justes do login e register

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,85 +8,76 @@ import logoWhite from "@/app/assets/logo_white.png";
 import Link from "next/link";
 import Input from "../components/input/input";
 import Button from "../components/button/button";
-import { 
-  MdVisibility, 
-  MdVisibilityOff 
-} from "react-icons/md";
+import "./styles.scss";
+import { MdVisibility, MdVisibilityOff } from "react-icons/md";
 import { useMemo, useState } from "react";
 
 export default function Login() {
   const [show, setShow] = useState<boolean>(false);
   const [emailCpf, setEmailCpf] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const [openModal, setOpenModal] = useState<boolean>(false)
+  const [openModal, setOpenModal] = useState<boolean>(false);
 
   const validInputs = useMemo(() => {
     // memorization para validação dos campos de email / cpf e senha
-  }, [])
-
+  }, []);
 
   // Função para fazer login no sistema
-  const handleLogin = () => {
-
-  }
-  
+  const handleLogin = () => {};
 
   return (
-    <div className="">
-      {openModal && (
-        <div>
-          Aqui vai o modal de troca de senha
-        </div>
-      )}
-      <Banner id="banner" className="banner">
-        <Image src={imgBanner} alt="Logo banner" width={150} height={150} />
+    <div className="container">
+      {openModal && <div>Aqui vai o modal de troca de senha</div>}
+      <Banner id="banner-container" className="banner-container">
+        <Image src={imgBanner} alt="Logo banner" style={{ width: "100%" }} />
       </Banner>
 
-      <div className="form-container">
-        <Link href="/">
-          <Image src={logoWhite} alt="Logo" />
-        </Link>
+      <div className="login">
+        <div className="form-container">
+          <Link href="/">
+            <Image
+              src={logoWhite}
+              alt="Logo"
+              style={{ margin: "0 auto", padding: "1rem" }}
+            />
+          </Link>
+          <Form id="form-login" className="form-login">
+            <div className="input-container">
+              <Input
+                placeholder="Email / CPF"
+                type="text"
+                id="cpf"
+                className="cpf"
+                onChange={setEmailCpf}
+                value={emailCpf}
+              />
+              <Input
+                placeholder="Senha"
+                type="password"
+                id="password"
+                className="password"
+                onChange={setPassword}
+                value={password}
+              />
+              {show && <MdVisibility onClick={() => setShow(true)} />}
+              {!show && <MdVisibilityOff onClick={() => setShow(false)} />}
+              <small>0 / 15</small>
+              <Link href="#" onClick={() => setOpenModal(true)}>
+                ESQUECEU SUA SENHA?
+              </Link>
+            </div>
 
-        <div className="title">
-          <h1>Login</h1>
-          <p>
-            Ainda não tem uma conta? <Link href="/register">Cadastrar</Link>
-          </p>
+            <Button type="submit" className="btn-login" onClick={handleLogin}>
+              ENTRAR
+            </Button>
+            <div className="title">
+              <p>
+                Ainda não tem uma conta? <Link href="/register">Cadastrar</Link>
+              </p>
+            </div>
+          </Form>
         </div>
-        <Form id="form-login" className="form-login">
-          <div className="">
-            <Input
-              placeholder="Email / CPF"
-              type="text"
-              id="cpf"
-              className="cpf"
-              onChange={setEmailCpf}
-              value={emailCpf}
-            />
-            <Input
-              placeholder="Senha"
-              type="password"
-              id="password"
-              className="password"
-              onChange={setPassword}
-              value={password}
-            />
-            {show && (
-              <MdVisibility onClick={() => setShow(true)} />
-            )}
-            {!show && (
-              <MdVisibilityOff onClick={() => setShow(false)} />
-            )}
-            <small>0 / 15</small>
-            <Link href="#" onClick={() => setOpenModal(true)}>ESQUECEU SUA SENHA?</Link>
-          </div>
-
-          <Button type="submit" className="btn-login" onClick={handleLogin}>
-            ENTRAR
-          </Button>
-        </Form>
       </div>
     </div>
   );
-};
-
+}
