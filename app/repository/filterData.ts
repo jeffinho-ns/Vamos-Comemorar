e685f188dd62baf6e4243564fb238a90b7a917fa
Tiago Borges <tiagoborges899@gmail.com>
2024-06-25 18:32:27 -0300

    feat: criação do arquivo de repository

---
+++ app/repository/filterData.ts
@@ -0,0 +1,18 @@
+export const filterData = [
+    "Empresa",
+    "Icone",
+    "Nome",
+    "Convidados",
+    "Criado em",
+    "Ações"
+]
+
+export const users = [
+    {
+      name: "User 01",
+      email: "user@hotmail.com",
+      telefone: "2199999999",
+      status: "pendente",
+      createAt: "16/06/2024"
+    }
+  ]
