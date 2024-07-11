b1269d4a5be2c55cedb09330d42965dc02f3a138
Jefferson Lima <82480661+jeffinho-ns@users.noreply.github.com>
2024-07-11 16:30:03 -0300

    Refinando os ajustes na pagina mobile

--- app/webapp/page.tsx
+++ app/webapp/page.tsx
@@ -1,17 +1,15 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-import "react-multi-carousel/lib/styles.css";
 import "../webapp/global.scss";
 import { MdSearch } from "react-icons/md";
 import { FaHeart } from "react-icons/fa";
 
 import Header from "./components/header/header";
 import Form from "./components/form/form";
-import Carousel from "react-multi-carousel";
 import Footer from "./components/footer/footer";
 
 import img01 from "@/app/webapp/assetsMobile/justino.png";
@@ -19,36 +17,10 @@ import img02 from "@/app/webapp/assetsMobile/ohfregues.png";
 import img03 from "@/app/webapp/assetsMobile/pracinha.png";
 
 export default function Home() {
-  const [showSecondCarousel, setShowSecondCarousel] = useState(false);
-
-  const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
-
-  const Card = ({
-    image,
-    title,
-    address,
-    link
-  }) => (
+  const Card = ({ image, title, address, link }) => (
     <Link href={link}>
       <motion.div
-        className="relative bg-white rounded-lg shadow-md overflow-hidden mx-4 mt-4 card-container"
+        className="relative bg-white rounded-lg shadow-md overflow-hidden card-container"
         initial={{ opacity: 0, x: -100 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.5 }}
@@ -77,6 +49,9 @@ export default function Home() {
   return (
     <>
       <Header />
+      <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
       <p className="title">Qual evento você procura? </p>
       <div className="flex justify-center w-full px-4 py-4">
         <Form
@@ -87,7 +62,7 @@ export default function Home() {
             <div className="flex items-center w-full relative">
               <MdSearch className="text-blue-600 text-2xl absolute right-2" />
               <input
-                placeholder="Buscar por nome"
+                placeholder="Buscar eventos"
                 type="text"
                 value=""
                 id="data"
@@ -98,7 +73,7 @@ export default function Home() {
         </Form>
       </div>
       <main className="container mx-auto px-4 bg-white pb-8">
-        <Carousel responsive={responsive} className="grid">
+        <div className="cards-container">
           <Card
             image={img01}
             title="Seu Justino"
@@ -117,7 +92,7 @@ export default function Home() {
             address="Vila Madalena - SP"
             link="/pracinha"
           />
-        </Carousel>
+        </div>
       </main>
       <Footer />
     </>
